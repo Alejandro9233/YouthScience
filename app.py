1be9d01ab94af5511c15eeef69b54c668f87618c
Alejandro Barrera <114511645+Alejandro9233@users.noreply.github.com>
2023-09-12 03:27:01 +0000

    x

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ def us():
     areas = {1,2,3,4,5,6}
     return render_template("nosotros.html", areas = areas)
 
+@app.route("/test")
+def a():
+    return render_template('test.html')
+
+@app.route("/post")
+def post():
+    return 0
+
 
 if __name__ == '__main__':
     app.run(debug = True)
